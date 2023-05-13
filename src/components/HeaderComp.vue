b3259276a2683669e157b06af61a0cd673fd3250
yamadicu <dicutia@gmail.com>
2023-05-13 14:04:47 +0200

    fine header

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -36,6 +36,7 @@ export default {
             </div>
         </div>
 
+        <!-- navbar con link per altre pagine -->
         <nav class="mt-4">
             <div class="container cont-nav">
 
@@ -67,12 +68,61 @@ export default {
                             <a href="#">blog</a>
                         </li>
                         <li class="ms-1">
-                            <a href="#" id="get-it">get in touch</a>
+                            <a href="#" class="get-in">get in touch</a>
                         </li>
                     </ul>
                 </div>
             </div>
         </nav>
+
+
+        <div class="d-flex container">
+
+            <div class="col-9"></div>
+
+            <div class="col-3 cont-iscr">
+
+                <div id="iscrizione">
+
+                    <span>fusions & acquisitions</span>
+                    <h1 class="fw-bolder">Insurance Consulting</h1>
+
+                    <div class="d-flex flex-wrap">
+
+                        <!-- nome ed email -->
+                        <div class="d-flex m-1">
+
+                            <input type="text" class="form-control me-2 bg-inp" placeholder="Name">
+                            <input type="email" class="form-control me-2 bg-inp" placeholder="Email">
+
+                        </div>
+
+                        <div class="d-flex m-1">
+
+                            <input type="type" class="form-control me-2 bg-inp" placeholder="Phone">
+
+                            <div>
+                                <button type="button" class="form-control me-4 bg-inp dropdown-toggle"
+                                    data-bs-toggle="dropdown" aria-expanded="false">
+                                    Dropdown
+                                </button>
+                                <ul class="dropdown-menu">
+                                    <li><a class="dropdown-item" href="#">Home</a></li>
+                                    <li><a class="dropdown-item" href="#">About</a></li>
+                                </ul>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="mt-4">
+                        <span class="get-in">gen in touch</span>
+                        <span id="read">reade more</span>
+                    </div>
+
+                </div>
+            </div>
+        </div>
 
     </section>
 </template>
@@ -144,14 +194,48 @@ section {
 
                         }
 
-                        #get-it {
+                        .get-in {
                             color: white;
                             background-color: rgb(5 130 131);
                             padding: 10px;
-                            border-radius: 10px;
+                            border-radius: 5px;
                         }
                     }
                 }
+            }
+        }
+    }
+
+    .cont-iscr {
+
+        float: inline-end;
+
+        #iscrizione {
+            margin-top: 100px;
+
+            span {
+                color: rgb(0 166 166);
+                text-transform: uppercase;
+                font-weight: bolder;
+            }
+
+            .bg-inp {
+                background-color: rgb(209 213 214);
+            }
+
+            .get-in {
+                color: white;
+                background-color: rgb(5 130 131);
+                padding: 10px;
+                border-radius: 5px;
+            }
+
+            #read {
+                color: rgb(5 130 131);
+                padding: 10px;
+                border: 1px solid rgb(5 130 131);
+                border-radius: 5px;
+                margin-left: 10px;
             }
         }
     }
